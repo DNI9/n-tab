0019cdbac91953b1a8df6682196e20b38e53e723
DNI9 <itsr9boi@gmail.com>
2020-06-26 20:44:49 +0530

    upto time

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 // https://developer.chrome.com/extensions/getstarted
+//
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+
+const setTime = () => {
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    if (hour > 12) hour -= 12;
+    if (minute < 10) minute = '0' + minute;
+    hours.textContent = hour;
+    minutes.textContent = minute;
+};
+
+setInterval(setTime, 500);
